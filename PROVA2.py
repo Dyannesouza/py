03ef180d23af3d4e71178b7fc57fddb6483f1f23
Dyannesouza <dyanne.souza@outlook.com>
2025-06-29 22:12:20 -0300

    Add files via upload

---
+++ PROVA2.py
@@ -0,0 +1,8 @@
+numero = float(input("digite um número: "))
+
+if numero < 0:
+    str(print("este número é negativo"))
+elif numero > 0:
+   str(print("este número é positivo"))
+elif numero == 0:
+    str(print("este número é 0"))
